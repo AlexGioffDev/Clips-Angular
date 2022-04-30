359df402e7d8cc800cae7fa915e75b106df6ce40
AlexGioffDev <alex.gioffre_dev@outlook.it>
2022-04-30 16:41:06 +0200

    Project complete

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAZKzOnGYkFGxsPuErDTFqBTUWSoArk4PU",
+    authDomain: "clips-46552.firebaseapp.com",
+    projectId: "clips-46552",
+    storageBucket: "clips-46552.appspot.com",
+    messagingSenderId: "242936423564",
+    appId: "1:242936423564:web:094897085b5895a50d07a9"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAZKzOnGYkFGxsPuErDTFqBTUWSoArk4PU",
+    authDomain: "clips-46552.firebaseapp.com",
+    projectId: "clips-46552",
+    storageBucket: "clips-46552.appspot.com",
+    messagingSenderId: "242936423564",
+    appId: "1:242936423564:web:094897085b5895a50d07a9"
+  }
 };
 
 /*
